54f9cc0738e09437ce5f564478614ea67adcaf34
Zairth <mano.gimenez@hotmail.com>
2024-11-27 15:54:28 +0100

    Trying to fix error

--- _printf.c
+++ _printf.c
@@ -161,7 +161,48 @@ int print_int(va_list args)
  */
 int print_hexa(va_list args)
 {
+	int MIN_hit = 0;
+	int count = 8;
+	int modulo = 1000000000;
+	int division = 100000000;
 	int n = va_arg(args, int);
-	_putchar(n);
+
+	if (n == -2147483648)
+	{
+		n++;
+		MIN_hit++;
+	}
+	if (n < 0)
+	{
+		n = n * (-1);
+		_putchar('-');
+	}
+	if (n != 0)
+	{
+		if (n > modulo)
+		{
+			_putchar((n / 1000000000) + '0');
+		}
+		else
+		{
+			while (modulo / 10 > n)
+			{
+				modulo /= 10;
+				division /= 10;
+				count--;
+			}
+		}
+		while (count != 0)
+		{
+			_putchar(((n % modulo) / division) + '0');
+			modulo /= 10;
+			division /= 10;
+			count--;
+		}
+		n %= 10;
+	}
+	if (MIN_hit == 1)
+		n++;
+	_putchar(n + '0');
 	return (0);
 }
